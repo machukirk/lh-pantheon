3a0e1ffa511736578918c559f2cf788adabf8746
mattalmanac <matt@almanac.digital>
2023-05-19 17:52:25 +0100

    Fixed child theme stylesheet. Fixed pantheon php version

--- wp-content/themes/leaf-home/functions.php
+++ wp-content/themes/leaf-home/functions.php
@@ -1,6 +1,19 @@
 <?php
 
-function leaf_home_styles() {
-	wp_enqueue_style( 'leaf-home-style', get_stylesheet_uri(), array( 'twentytwentythree' ) );
-}
 add_action( 'wp_enqueue_scripts', 'leaf_home_styles' );
+
+function leaf_home_styles() {
+	$parenthandle = 'parent-style';
+
+	wp_enqueue_style(
+		$parenthandle,
+		get_template_directory_uri() . '/style.css',
+		array(),
+	);
+
+	wp_enqueue_style(
+		'leaf-home-style',
+		get_stylesheet_uri(),
+		array( $parenthandle )
+	);
+}
